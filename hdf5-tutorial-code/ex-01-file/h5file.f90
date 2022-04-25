10e411268f61edcaf2906e96a2a7b1e1d2087b5f
Ju Liu <liujuy@gmail.com>
2022-04-25 09:38:18 +0800

    example hdf5 in

---
+++ hdf5-tutorial-code/ex-01-file/h5file.f90
@@ -0,0 +1,29 @@
+PROGRAM FILEEXAMPLE
+
+  USE HDF5 ! This module contains all necessary modules 
+
+  IMPLICIT NONE
+
+  CHARACTER(LEN=8), PARAMETER :: filename = "filef.h5" ! File name
+  INTEGER(HID_T) :: file_id                            ! File identifier
+
+  INTEGER     ::   error  ! Error flag
+
+  !
+  !    Initialize FORTRAN interface.
+  !
+  CALL h5open_f (error)
+  !
+  ! Create a new file using default properties.
+  ! 
+  CALL h5fcreate_f(filename, H5F_ACC_TRUNC_F, file_id, error)
+
+  !
+  ! Terminate access to the file.
+  !
+  CALL h5fclose_f(file_id, error)
+  !
+  !    Close FORTRAN interface.
+  !
+  CALL h5close_f(error)
+END PROGRAM FILEEXAMPLE
