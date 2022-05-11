5a46813a7ec966a71b474d01d5ef988fce93e2fe
Ju Liu <liujuy@gmail.com>
2022-05-11 13:09:49 +0800

    script in

--- petsc-tutorial-code/ex5-ksp/ex5.c
+++ petsc-tutorial-code/ex5-ksp/ex5.c
@@ -1,49 +1,52 @@
-static char help[] = "Solves a tridiagonal linear system with KSP.\n\n";
+static char help[] = "Solves a tridiagonal linear system.\n\n";
 
 /*
   Include "petscksp.h" so that we can use KSP solvers.  Note that this file
   automatically includes:
-     petscsys.h    - base PETSc routines   petscvec.h - vectors
-     petscmat.h    - matrices              petscpc.h  - preconditioners
-     petscis.h     - index sets
-     petscviewer.h - viewers
+     petscsys.h       - base PETSc routines   petscvec.h - vectors
+     petscmat.h - matrices
+     petscis.h     - index sets            petscksp.h - Krylov subspace methods
+     petscviewer.h - viewers               petscpc.h  - preconditioners
 
-  Note:  The corresponding parallel example is ex23.c
+  Note:  The corresponding uniprocessor example is ex1.c
 */
 #include <petscksp.h>
 
 int main(int argc,char **args)
 {
-  Vec            x, b, u;      /* approx solution, RHS, exact solution */
-  Mat            A;            /* linear system matrix */
-  KSP            ksp;          /* linear solver context */
-  PC             pc;           /* preconditioner context */
-  PetscReal      norm;         /* norm of solution error */
+  Vec            x, b, u;          /* approx solution, RHS, exact solution */
+  Mat            A;                /* linear system matrix */
+  KSP            ksp;              /* linear solver context */
+  PC             pc;               /* preconditioner context */
+  PetscReal      norm,tol=1000.*PETSC_MACHINE_EPSILON;  /* norm of solution error */
   PetscErrorCode ierr;
-  PetscInt       i,n = 10,col[3],its;
-  PetscMPIInt    size;
-  PetscScalar    value[3];
+  PetscInt       i,n = 10,col[3],its,rstart,rend,nlocal;
+  PetscScalar    one = 1.0,value[3];
 
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
-  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRMPI(ierr);
-  if (size != 1) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_WRONG_MPI_SIZE,"This is a uniprocessor example only!");
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
          Compute the matrix and right-hand-side vector that define
          the linear system, Ax = b.
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
   /*
      Create vectors.  Note that we form 1 vector from scratch and
-     then duplicate as needed.
+     then duplicate as needed. For this simple case let PETSc decide how
+     many elements of the vector are stored on each processor. The second
+     argument to VecSetSizes() below causes PETSc to decide.
   */
   ierr = VecCreate(PETSC_COMM_WORLD,&x);CHKERRQ(ierr);
-  ierr = PetscObjectSetName((PetscObject) x, "Solution");CHKERRQ(ierr);
   ierr = VecSetSizes(x,PETSC_DECIDE,n);CHKERRQ(ierr);
   ierr = VecSetFromOptions(x);CHKERRQ(ierr);
   ierr = VecDuplicate(x,&b);CHKERRQ(ierr);
   ierr = VecDuplicate(x,&u);CHKERRQ(ierr);
+
+  /* Identify the starting and ending mesh points on each
+     processor for the interior part of the mesh. We let PETSc decide
+     above. */
+  ierr = VecGetOwnershipRange(x,&rstart,&rend);CHKERRQ(ierr);
+  ierr = VecGetLocalSize(x,&nlocal);CHKERRQ(ierr);
 
   /*
      Create matrix.  When using MatCreate(), the matrix format can
@@ -52,41 +55,63 @@ int main(int argc,char **args)
      Performance tuning note:  For problems of substantial size,
      preallocation of matrix memory is crucial for attaining good
      performance. See the matrix chapter of the users manual for details.
+
+     We pass in nlocal as the "local" size of the matrix to force it
+     to have the same parallel layout as the vector created above.
   */
   ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
-  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,n,n);CHKERRQ(ierr);
+  ierr = MatSetSizes(A,nlocal,nlocal,n,n);CHKERRQ(ierr);
   ierr = MatSetFromOptions(A);CHKERRQ(ierr);
   ierr = MatSetUp(A);CHKERRQ(ierr);
 
   /*
-     Assemble matrix
+     Assemble matrix.
+
+     The linear system is distributed across the processors by
+     chunks of contiguous rows, which correspond to contiguous
+     sections of the mesh on which the problem is discretized.
+     For matrix assembly, each processor contributes entries for
+     the part that it owns locally.
   */
+  if (!rstart) {
+    rstart = 1;
+    i      = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
+    ierr   = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  if (rend == n) {
+    rend = n-1;
+    i    = n-1; col[0] = n-2; col[1] = n-1; value[0] = -1.0; value[1] = 2.0;
+    ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+  /* Set entries corresponding to the mesh interior */
   value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
-  for (i=1; i<n-1; i++) {
+  for (i=rstart; i<rend; i++) {
     col[0] = i-1; col[1] = i; col[2] = i+1;
     ierr   = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
   }
-  i    = n - 1; col[0] = n - 2; col[1] = n - 1;
-  ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
-  i    = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
-  ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+
+  /* Assemble the matrix */
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
 
   /*
      Set exact solution; then compute right-hand-side vector.
   */
-  ierr = VecSet(u,1.0);CHKERRQ(ierr);
+  ierr = VecSet(u,one);CHKERRQ(ierr);
   ierr = MatMult(A,u,b);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
                 Create the linear solver and set various options
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Create linear solver context
+  */
   ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
 
   /*
      Set operators. Here the matrix that defines the linear system
-     also serves as the matrix that defines the preconditioner.
+     also serves as the preconditioning matrix.
   */
   ierr = KSPSetOperators(ksp,A,A);CHKERRQ(ierr);
 
@@ -101,7 +126,7 @@ int main(int argc,char **args)
   */
   ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
   ierr = PCSetType(pc,PCJACOBI);CHKERRQ(ierr);
-  ierr = KSPSetTolerances(ksp,1.e-5,PETSC_DEFAULT,PETSC_DEFAULT,PETSC_DEFAULT);CHKERRQ(ierr);
+  ierr = KSPSetTolerances(ksp,1.e-7,PETSC_DEFAULT,PETSC_DEFAULT,PETSC_DEFAULT);CHKERRQ(ierr);
 
   /*
     Set runtime options, e.g.,
@@ -115,21 +140,23 @@ int main(int argc,char **args)
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
                       Solve the linear system
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Solve linear system
+  */
   ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
 
-  /*
-     View solver info; we could instead use the option -ksp_view to
-     print this info to the screen at the conclusion of KSPSolve().
-  */
-  ierr = KSPView(ksp,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-                      Check the solution and clean up
+                      Check solution and clean up
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Check the error
+  */
   ierr = VecAXPY(x,-1.0,u);CHKERRQ(ierr);
   ierr = VecNorm(x,NORM_2,&norm);CHKERRQ(ierr);
   ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"Norm of error %g, Iterations %D\n",(double)norm,its);CHKERRQ(ierr);
+  if (norm > tol) {
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Norm of error %g, Iterations %D\n",(double)norm,its);CHKERRQ(ierr);
+  }
 
   /*
      Free work space.  All PETSc objects should be destroyed when they
@@ -149,33 +176,4 @@ int main(int argc,char **args)
   return ierr;
 }
 
-/*TEST
-
-   test:
-      args: -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always
-
-   test:
-      suffix: 2
-      args: -pc_type sor -pc_sor_symmetric -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always
-
-   test:
-      suffix: 2_aijcusparse
-      requires: cuda
-      args: -pc_type sor -pc_sor_symmetric -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -mat_type aijcusparse -vec_type cuda
-
-   test:
-      suffix: 3
-      args: -pc_type eisenstat -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always
-
-   test:
-      suffix: 3_aijcusparse
-      requires: cuda
-      args: -pc_type eisenstat -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -mat_type aijcusparse -vec_type cuda
-
-   test:
-      suffix: aijcusparse
-      requires: cuda
-      args: -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -mat_type aijcusparse -vec_type cuda
-      output_file: output/ex1_1_aijcusparse.out
-
-TEST*/
+// EOF
---
+++ petsc-tutorial-code/ex5-ksp/run_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#mpirun -np 2 ./ex5.out -mat_view -ksp_view
+
+#mpirun -np 2 ./ex5.out -ksp_type cg -pc_type none  -ksp_monitor  -ksp_view
+
+mpirun -np 3 ./ex5.out -ksp_type gmres \
+  -ksp_gmres_restart 30 -ksp_rtol 1.0e-10 \
+  -ksp_atol 1.0e-50 -ksp_max_it 1500 \
+  -ksp_gmres_modifiedgramschmidt \
+  -pc_type asm \
+  -ksp_rtol 1.0e-10 -sub_ksp_type richardson \
+  -sub_pc_type icc -ksp_monitor_short \
+  -ksp_converged_reason \
+  -ksp_view
